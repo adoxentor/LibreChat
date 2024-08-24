328f25d2f04c6223fccfc2b4b7fa38535706b061
Iddo <admin@adox.dev>
2024-08-24 03:46:49 +0300

    for copying from dark mode into html supported apps (for example google docs, we should remove the color styles)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,19 @@ import * as RadixToast from '@radix-ui/react-toast';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider, QueryCache } from '@tanstack/react-query';
-import { ScreenshotProvider, ThemeProvider, useApiErrorBoundary } from './hooks';
+import {
+  ScreenshotProvider,
+  ThemeProvider,
+  useApiErrorBoundary,
+  useColorStrippingCopy,
+} from './hooks';
 import { ToastProvider } from './Providers';
 import Toast from './components/ui/Toast';
 import { router } from './routes';
 
 const App = () => {
   const { setError } = useApiErrorBoundary();
-
+  useColorStrippingCopy();
   const queryClient = new QueryClient({
     queryCache: new QueryCache({
       onError: (error) => {
